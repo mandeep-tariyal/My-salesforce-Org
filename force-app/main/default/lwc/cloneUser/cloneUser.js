50dbe3a71b3506a19d639bf6dd2413f5c8647046
mandeep-tariyal <tariyalmandeep@gmail.com>
2022-06-21 12:41:40 +0530

    Paste from trail

--- force-app/main/default/lwc/cloneUser/cloneUser.js
+++ force-app/main/default/lwc/cloneUser/cloneUser.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class CloneUser extends LightningElement {}
+export default class CloneUser extends LightningElement {
+    greeting = 'World';
+  changeHandler(event) {
+    this.greeting = event.target.value;
+  }
+    
+}
